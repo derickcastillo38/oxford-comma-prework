eaa37bcb03481a5f98f795e47a1173c17ab16e6a
derickcastillo38 <9derick@gmail.com>
2018-06-22 02:04:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif array.size == 3
     "#{array.first}, #{array[1]}, and #{array.last}"
   else
-    "#{array.first}, #{array.second}, #{array[2]}, #{array[3]}, and #{array.last}"
+    "#{array.first}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array.last}"
   end
 end

e6aec381de01d73883ecdd856423e3945d9042eb
derickcastillo38 <9derick@gmail.com>
2018-06-22 02:04:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.size == 1
     array.first
   elsif array.size == 2
-    "#{array.first} and #{array.second}"
+    "#{array.first} and #{array.last}"
   elsif array.size == 3
-    "#{array.first}, #{array.second}, and #{array.last}"
+    "#{array.first}, #{array[1]}, and #{array.last}"
   else
     "#{array.first}, #{array.second}, #{array[2]}, #{array[3]}, and #{array.last}"
   end
